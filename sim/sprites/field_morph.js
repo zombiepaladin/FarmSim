916f8de0ee4d7129f327808607e7ecb09862e925
bwheel <bwheel1990@gmail.com>
2014-03-10 11:28:58 -0500

    Committing to push changes

--- sim/sprites/field_morph.js
+++ sim/sprites/field_morph.js
@@ -8,8 +8,9 @@ FieldNodeMorph.prototype = new Morph();
 FieldNodeMorph.prototype.constructor = new Morph();
 FieldNodeMorph.uber =  Morph.prototype;
 
-FieldNodeMorph.innerRadius = 5;  
-FieldNodeMorph.outerRadius = 10;
+FieldNodeMorph.prototype.innerRadius = 5;  
+FieldNodeMorph.prototype.outerRadius = 10;
+FieldNodeMorph.prototype.border = 2;
 
 function FieldNodeMorph(point, nextNode, globals) {
 	this.init(point, nextNode, globals);
@@ -46,7 +47,7 @@ FieldNodeMorph.prototype.drawNode = function( context ) {
 	}
 };
 
-FieldNodeMorph.prototype.NodeWasClicked = function( x_click, y_click ) {
+FieldNodeMorph.prototype.nodeWasClicked = function( x_click, y_click ) {
 	
 	var dx = Math.abs( x_click - this.x);
 	var dy = Math.abs( y_click - this.y);
@@ -55,6 +56,76 @@ FieldNodeMorph.prototype.NodeWasClicked = function( x_click, y_click ) {
 	
 	 return (test <= this.outerRadius ) ? true : false;
 };
+
+FieldNodeMorph.prototype.lineWasClicked1 = function( x_click, y_click, nextNode) {
+	
+	var startPoint = new Point(this.x, this.y),
+		queryPoint = new Point( x_click, y_click),
+		endPoint = new Point( nextNode.x, nextNode.y);
+
+	// check that the cross product is not zero
+	var crossproduct = (queryPoint.y - startPoint.y) * (endPoint.x - startPoint.x) - ( queryPoint.x - startPoint.x) * ( endPoint.y - startPoint.y);	
+	if( Math.abs(crossproduct) > 0.000000000000000111)  return false; // it is not greater than epsilon for double precesion.		
+	
+	// check that the dot product is positive
+	var dotproduct = (queryPoint.y - startPoint.y) * (endPoint.x - startPoint.x) + (queryPoint.x - startPoint.x) * (endPoint.y - startPoint.y);
+	if( dotproduct < 0)  return false;
+
+	// check that the dot product is less than the squared distance between start to finish
+	var squareLen = ( endPoint.x - startPoint.x) * 	( endPoint.x - startPoint.x) + ( endPoint.y - startPoint.y) *( endPoint.y - startPoint.y) 
+	if( dotproduct > squareLen )  return false;
+
+	return true;
+};
+
+FieldNodeMorph.prototype.lineWasClicked2 = function( x_click, y_click, nextNode) {
+	
+	var m, 
+		b, 
+		test, 
+	    dist1, 
+		dist2, 
+		distTotal;
+	
+	for( i = -20; i < 20; i++)
+	{
+		m = ( nextNode.y - this.y )/ ( nextNode.x - (this.x+i) );
+		b = this.y/(m*(this.x+i));
+		test = m*x_click + b;
+	
+		dist1 = Math.sqrt( ( x_click - (this.x+i))*( x_click - (this.x+i)) + ( y_click - this.y)*( y_click - this.y) );
+		dist2 = Math.sqrt( ( nextNode.x - x_click )*( nextNode.x - x_click ) + ( nextNode.y - y_click )*( nextNode.y - y_click ) );
+		distTotal = Math.sqrt( (nextNode.x - (this.x+i)) * (nextNode.x - (this.x+i)) + (nextNode.y - this.y) * (nextNode.y - this.y) );
+		if( Math.abs(distTotal - dist1 - dist2) > 0.000000000000001) continue;
+	
+		if( (test - y_click) > 0.0000000000000001) continue;
+	
+		return true;
+	}
+	return false
+	
+};
+
+FieldNodeMorph.prototype.lineWasClicked = function( x_click, y_click, nextNode) {
+	
+	
+	for( var i = x_click - 10; i < x_click + 10; i++)
+	{
+		for( var j = y_click - 10; j < y_click + 10; j++)
+		{
+			if(this.lineWasClicked1( i,j,nextNode) ) {
+				return true;
+			}
+			
+			
+		}
+		
+	}
+	
+	return false;
+	
+};
+
 
 FieldNodeMorph.prototype.drawNew = function() {
 
@@ -117,25 +188,35 @@ FieldMorph.prototype.fixLayout = function() {
 FieldMorph.prototype.mouseDownLeft = function(pos) {
 	
 	var myself = this;
+	var x_click = pos.x - myself.bounds.origin.x;
+	var y_click = pos.y - myself.bounds.origin.y;
+	var exit = false;
 	switch( this.state)
 	{
 		case "idle":			
 			this.boundary.forEach( function( node ) {
 				
-				if( node.NodeWasClicked( pos.x - myself.bounds.origin.x, pos.y - myself.bounds.origin.y ) ) 
+				if( !exit)
 				{
-					selectedNode = node;
-					myself.state = "dragging";
+					if( node.nodeWasClicked( x_click, y_click ) ) 
+					{
+						myself.selectedNode = node;
+						myself.state = "dragging";
+						console.log("dragging click");
+						exit = true;
+					}
+					else if( node.lineWasClicked( x_click, y_click, (node.next) ? node.next : myself.boundary[0] ) )
+					{
+						console.log( "found true");
+						// create new node
+						myself.selectedNode = node;
+						myself.addNode( x_click , y_click, (node.next) ? node.next : myself.boundary[0] )
+						myself.slectedNode = node.next;
+						myself.state = "dragging"
+						myself.refresh();
+						exit = true;
+					}
 				}
-				/*
-				else if( node.LineWasClicked( pos.x - myself.bounds.origin.x, pos.y - myself.bounds.origin.y ) )
-				{
-					// create new node
-					// newNode = addNode(x,y)
-					// slectedNode = newNode;
-					// myself.state = "dragging"
-				}
-				*/
 			});
 			
 			/* 
@@ -145,36 +226,56 @@ FieldMorph.prototype.mouseDownLeft = function(pos) {
 			}
 			*/
 			
-			var idle;
 			break;
 		case "dragging":
 		case "moving":
-		this.state = "idle";
-		var drag, 
-			move;
-		break;
+			this.state = "idle";
+			break;
 	
 	}
 };
 
+FieldMorph.prototype.refresh = function() {
+	var myself = this;
+	myself.hide();
+	myself.drawNew();
+	myself.show();
+};
+
+FieldMorph.prototype.addNode = function( x, y , next) {
+	
+	var myself = this;
+	
+	var newNode = new FieldNodeMorph( new Point( x,y), next);
+	myself.selectedNode.next = newNode;
+	myself.boundary.splice( myself.boundary.indexOf( myself.selectedNode ) + 1 , 0 , newNode);
+	
+	
+};
+
 FieldMorph.prototype.mouseMove = function(pos) {
 	
+	var myself = this;
+	var x = (pos.x - myself.bounds.origin.x);
+	var y = (pos.y - myself.bounds.origin.y);
+	console.log(" mouse move x="+ x  + " y="+y );
 	
 	switch( this.state )
 	{
 		case "idle":
 			// doing nothing.
-			var idle;
 		break;
 		
 		case "dragging":
 			// dragging a node around
-			var drag;
+			myself.selectedNode.x = pos.x - myself.bounds.origin.x;
+			myself.selectedNode.y = pos.y - myself.bounds.origin.y;
+			console.log("dragging move x =" + myself.selectedNode.x + " y=" + myself.selectedNode.y);
+			myself.refresh();
 		break;
 		
 		case "moving":
 			// moving the whole field
-			var move;
 		break;
 		
 		
@@ -183,6 +284,25 @@ FieldMorph.prototype.mouseMove = function(pos) {
 
 }
 
+
+FieldMorph.prototype.mouseClickLeft = function( pos ) {
+	
+	console.log( " mouseClickLeft x=" + pos.x + " y=" + pos.y);
+	switch( this.state )
+	{
+		case "idle":
+		break;
+		case "dragging":
+			this.state = "idle";
+		break;
+		case "moving":
+			this.state = "idle";
+		break;
+		
+	}
+	
+	
+};
 
 
 /*
